28b60e1e20712b559fe2304aa8dcd0cf91b93489
R1ZQ1R4 <aqirae63@gmail.com>
2017-07-29 04:14:50 +0700

    login register v2, OOP style + minifed code v1

--- content/home.php
+++ content/home.php
@@ -1,3 +1,26 @@
 <?php
 
 ?>
+
+<nav class="navbar">
+	<div class="navbar main">
+	<a class="navbar-brand" href="index.php">SMKN 1 Bangil</a>
+	</div>
+
+	<div class="navbar sub">
+
+		<a href="#">Home</a>
+		<a href="#">News</a>
+		<a href="#">Destination</a>
+		<?php if (isset($_SESSION['name'])): ?>
+		<a href="?page=admin">Dashboard</a>
+		<a href="?page=profil"><?= $_SESSION['name'] ?></a>
+		<a href="?page=logout">Logout</a>
+		<?php else: ?>
+		<a href="?page=login">Login</a>
+		<?php endif ?>
+
+	
+	</div>
+</nav>
+<header></header>
